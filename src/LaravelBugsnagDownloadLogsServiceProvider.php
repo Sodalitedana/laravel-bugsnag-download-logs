9b804bdf596015e9d16d4fd338d0637414906cf7
sodalitedana <sodalite.dana@gmail.com>
2025-09-25 11:58:48 +0200

    f

--- src/LaravelBugsnagDownloadLogsServiceProvider.php
+++ src/LaravelBugsnagDownloadLogsServiceProvider.php
@@ -10,21 +10,18 @@ class LaravelBugsnagDownloadLogsServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-bugsnag-download-logs')
             ->hasConfigFile()
             ->hasCommand(LaravelBugsnagDownloadLogsCommand::class);
     }
 
-    //    public function boot(): void
-    //    {
-    //        $this->publishes([
-    //            __DIR__.'/../config/laravel-bugsnag-download-logs.php' => config_path('bugsnag-errors.php'),
-    //        ]);
-    //    }
+//        public function boot(): void
+//        {
+//            $this->publishes([
+//                __DIR__.'/../config/laravel-bugsnag-download-logs.php' => config_path('laravel-bugsnag-download-logs.php'),
+//            ]);
+//        }
+
+
 }
